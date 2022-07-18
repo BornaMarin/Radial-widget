5335a66e4c099ad0ce944b41a49514ae76093034
BornaMarin <bornamarin3232@gmail.com>
2022-07-18 14:29:36 +0200

    feat: canvas context core

---
+++ src/components/canvas/canvasCore/canvasContext.tsx
@@ -0,0 +1,40 @@
+import React, { useContext, useRef, useState } from 'react';
+const CanvasContext = React.createContext({});
+
+interface CanvasChildren {
+  children: JSX.Element[] | JSX.Element;
+  draw: () => void;
+}
+
+export const CanvasProvider = (props: CanvasChildren): JSX.Element => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const prepareCanvas = () => {
+    let canvas: HTMLCanvasElement | null = null;
+    if (canvasRef.current) {
+      canvas = canvasRef.current;
+    }
+    let context: CanvasRenderingContext2D | null = null;
+    if (canvas) {
+      context = canvas.getContext('2d');
+    }
+    const dpr = window.devicePixelRatio;
+
+    if (context) {
+      context.scale(dpr, dpr);
+      context.lineCap = 'round';
+      context.strokeStyle = 'black';
+      context.lineWidth = 5;
+    }
+  };
+  return (
+    <CanvasContext.Provider
+      value={{
+        canvasRef,
+        prepareCanvas
+      }}>
+      {props.children}
+    </CanvasContext.Provider>
+  );
+};
+export const useCanvas = () => useContext(CanvasContext);
