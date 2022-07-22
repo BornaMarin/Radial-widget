ecff7a4402b52d5046d67b64c50e3e971931966c
BornaMarin <bornamarin3232@gmail.com>
2022-07-22 12:54:12 +0200

    feat: typo

--- src/components/canvas/models/RadialText.tsx
+++ src/components/canvas/models/RadialText.tsx
@@ -6,17 +6,17 @@ import { RadialWidgetText } from '../../../types/canvas/RadialWidgetText';
 import { IDrawableCanvasProvider } from '../../../types/interfaces/drawable.interface';
 
 export default function RadialText(props: IDonutValues) {
-  const [circle, setCircle] = useState(new RadialWidgetText(props));
+  const [radialText, setRadialText] = useState(new RadialWidgetText(props));
   const ref = React.useRef<IDrawableCanvasProvider>();
 
   useEffect(() => {
-    setCircle(new RadialWidgetText(props));
+    setRadialText(new RadialWidgetText(props));
     //todo check this.....
     setTimeout(() => ref?.current?.draw());
   }, [props]);
   return (
     <Canvas
-      drawable={circle}
+      drawable={radialText}
       ref={ref}
       zIndex={props.zIndex}
       xAxisStartingPoint={props.xAxisStartingPoint}
